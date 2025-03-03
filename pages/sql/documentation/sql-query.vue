1687dab0f1c7a9259a1d8c1b5492926332f42f2f
TuanPhamHP <phamanhtuan290896@gmail.com>
2025-03-03 17:01:17 +0700

    update

--- pages/sql/documentation/sql-query.vue
+++ pages/sql/documentation/sql-query.vue
@@ -379,6 +379,18 @@
 						<VCodeBlock :code="b22" highlightjs lang="sql" theme="atom-one-dark" />
 					</div>
 				</ClientOnly>
+				<p class="text-slate-900 dark:text-white leading-8">
+					Lý thuyết đủ rồi, làm
+					<a
+						href="/sql/practice/e_4"
+						target="_blank"
+						rel="noreferrer"
+						class="inline-block px-1 rounded text-slate-900 dark:text-white underline decoration-2 hover:text-cyan-500"
+					>
+						bài tập
+					</a>
+					nào.
+				</p>
 				<doc-next-page :pagination="pagePagination" />
 			</div>
 			<PageMarkBook />
