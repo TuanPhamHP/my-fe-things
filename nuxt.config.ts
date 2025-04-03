436e0638fad231a97948fe2fff2df9eefe944e1a
TuanPhamHP <phamanhtuan290896@gmail.com>
2025-04-03 14:39:48 +0700

    update: update mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
 			meta: [
 				{ charset: 'utf-8' },
 				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ name: 'color-scheme', content: 'light' },
 				{
 					name: 'description',
 					content:
